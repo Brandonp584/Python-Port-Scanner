6ddad34709e5881d8b49bbc8a225d44180f3b08e
Brandonp584 <Brandon.powell98@outlook.com>
2026-03-18 12:29:38 +1000

    Pro v2 scanner: added CLI, threading, banner detection, colored output, and optional file logging

--- scanner.py
+++ scanner.py
@@ -1,6 +1,10 @@
 import socket 
 import argparse
 from concurrent.futures import ThreadPoolExecutor
+from colorama import Fore, Style, init
+
+# Initialize colorama
+init(autoreset=True)
 
 # Argyment Parser
 parser = argparse.ArgumentParser(description="Python Port Scanner")
@@ -8,12 +12,13 @@ parser = argparse.ArgumentParser(description="Python Port Scanner")
 parser.add_argument("--target", type=str, default="127.0.0.1", help="Target IP address")
 parser.add_argument("--start", type=int, default=1, help="Start port")
 parser.add_argument("--end", type=int, default=6000, help="End port")
+parser.add_argument("--threads", type=int, default=100, help="Number of concurrent threads")
+parser.add_argument("--output", type=str, help="Optional: save results to a file")
 
 args = parser.parse_args()
 
 target = args.target
-start_port = args.start
-end_port = args.end
+ports = range(args.start, args.end + 1)
 
 # Common ports and their services
 common_ports = {
@@ -62,29 +67,34 @@ def port_scan( port):
         # 4. Check the result
         if result == 0:
             service = common_ports.get(port, "")
-
             banner = grab_banner(s, port)
 
             if banner != "Unknown":
-                print(f"[OPEN] Port {port} ({service}) - Banner: {banner}")
+                output = f"[OPEN] Port  {port} ({service}) - Banner: {banner}"
             elif service:
-                print(f"[OPEN] Port {port} ({service})")
+                output = f"[OPEN] port {port} ({service})"
             else:
-                print(f"[OPEN] Port {port} (Unknown)")
+                output = f"[OPEN] port {port} (Unknown)"
+
+
+            # Print coloured output
+            print(Fore.GREEN + output + Style.RESET_ALL)
+
+            # Optional: Save results to a file
+            if args.output:
+                with open(args.output, "a") as f:
+                    f.write(output + "\n")
         
         # 5. Close the connection
         s.close()
 
     except Exception as e:
-        print(f"An error occurred while scanning port {port}: {e}")
+        print(Fore.RED + f"Error scanning port {port}: {e}" + Style.RESET_ALL)
 
 # Run Scan
-print(f"Starting scan on {target}")
+print(Fore.CYAN + f"Starting scan on {target} with ports {args.start}-{args.end} using {args.threads} threads..." + Style.RESET_ALL)
 
-ports = range(start_port, end_port)
-
-# Limit Threads
-with ThreadPoolExecutor(max_workers=100) as executor:
+with ThreadPoolExecutor(max_workers=args.threads) as executor:
     executor.map(port_scan, ports)
 
-print("Scan completed.")
+print(Fore.CYAN + "Scan Complete." + Style.RESET_ALL)
